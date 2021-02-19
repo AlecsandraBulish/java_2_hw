7f8f66cb04d8921f1b287123bfa4d69eaed2d0a9
AlexBulish <alecsandra-bulish>
2021-02-19 16:46:25 +0300

    new directory chatbot

---
+++ src/ru/geekbrains/java2/chatBot/Client.java
@@ -0,0 +1,57 @@
+package ru.geekbrains.java2.chatBot;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client {
+    private static final int PORT = 882;
+    private static Socket socket;
+    private static final String IP_ADDRESS = "localhost";
+
+    public static void main(String[] args) {
+        try {
+            socket = new Socket(IP_ADDRESS, PORT);
+
+            while (true) {
+                DataInputStream in = new DataInputStream(socket.getInputStream());
+
+                String str1 = in.readUTF();
+
+                if (str1.equals("/end")) {
+                    System.out.println("Client disconnected");
+                    break;
+                }
+                System.out.println("Server: " + str1);
+
+
+                new Thread(() -> {
+                    try {
+                        while (true) {
+                            Scanner scannerIn = new Scanner(System.in);
+                            String str2 = scannerIn.nextLine();
+                            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+                            out.writeUTF(str2);
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }).start();
+
+
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
---
+++ src/ru/geekbrains/java2/chatBot/Server.java
@@ -0,0 +1,70 @@
+package ru.geekbrains.java2.chatBot;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Server {
+    private static final int PORT = 882;
+    private static ServerSocket server;
+    private static Socket socket;
+
+
+    public static void main(String[] args) {
+        try {
+            server = new ServerSocket(PORT);
+            System.out.println("Server started");
+            socket = server.accept();
+            System.out.println("Client connected");
+
+            while (true) {
+                Scanner scannerIn = new Scanner(System.in);
+                DataInputStream in = new DataInputStream(socket.getInputStream());
+
+                String str2 = scannerIn.nextLine();
+                DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+                out.writeUTF(str2);
+
+                Thread t1 = new Thread(() -> {
+                    try {
+                        while (true) {
+
+                            String str1 = in.readUTF();
+
+                            if (str1.equals("/end")) {
+                                System.out.println("Client disconnected");
+                                break;
+                            }
+                            System.out.println("Client: " + str1);
+                        }
+
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                });
+                t1.start();
+
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                socket.close();
+                server.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
